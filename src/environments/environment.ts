eb6efe34b5fb0fffd172bc1a346f8f152794cbb4
Abramcito-EP2 <abrahamycopilot@gmail.com>
2025-07-29 16:04:48 -0600

    v1.8.2 Actualizar la URL de la API en los entornos de desarrollo para apuntar al servidor correcto.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3333/api',
+  apiUrl: 'http://3.134.189.69/api',  // Puerto para la API
   wsUrl: 'http://localhost:3334'  // Puerto diferente para Socket.io
 };
